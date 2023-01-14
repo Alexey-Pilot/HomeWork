ea7384b00dd2b6ebbd1c0a5ab97bb5e2e51d4aac
Alexey <alexragnarek@gmail.com>
2023-01-14 20:43:51 +0500

    task4 is complited

---
+++ task_4/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int nMax = a;
+if (b > nMax)
+{  
+  nMax = b;
+}
+else if (c > nMax)
+{  
+  nMax = c;
+}
+int x = 0; // счетчик со списками и циклом for было бы бысрее, но еще не проходили
+if (a == nMax)
+{
+  x ++;
+}
+if (b == nMax)
+{
+  x ++;
+}
+if (c == nMax)
+{
+  x ++;
+}
+if (x == 1)
+{
+  Console.WriteLine($"Наибольшее число: {nMax}");
+}
+else if (x == 3)
+{
+  Console.WriteLine("Числа равны");
+}
+else
+{
+  Console.WriteLine($"Два наибольших числа равны: {nMax}");
+}
