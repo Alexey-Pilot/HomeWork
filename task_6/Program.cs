36e26ca76caa694594ac54d50d3e87a5365ac82e
Alexey <alexragnarek@gmail.com>
2023-01-14 20:53:47 +0500

    task 6 is complited

---
+++ task_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Input the number: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x % 2 == 0)
+{
+  Console.WriteLine($"Number {x} is even");
+}
+else 
+{
+  Console.WriteLine($"Nuber {x} is odd");
+}
